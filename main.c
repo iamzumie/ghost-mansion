a1badb4473ee17e185dca6da08a5f1c6cc314896
Michel Cuypers <mcu@tectumgroup.be>
2020-05-10 15:41:02 +0000

    Add locations & fixed user input

--- main.c
+++ main.c
@@ -1,12 +1,8 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <time.h>
 #include <string.h>
 #include <ctype.h>
-#ifdef _MSC_VER 
-//not #if defined(_WIN32) || defined(_WIN64) because we have strncasecmp in mingw
-#define strncasecmp _strnicmp
-#define strcasecmp _stricmp
-#endif
+#include <stdlib.h>
 
 // FUNCTIONS
 void command();
@@ -16,39 +12,57 @@ void function();
 void showmap();
 char* fWord();
 
+// LOCATIONS
+struct location {
+    const char *description;
+    const char *desc_upper;
+}
+locs[] = {
+    {""},
+    {"kitchen", "KITCHEN"},
+    {"dining room", "DINING"},
+    {"hall", "HALL"},
+    {"living room", "LIVING"},
+    {"toilet", "TOILET"},
+    {"upstairs", "UPSTAIRS"},
+};
+void loc_kitchen();
+void loc_living();
+void loc_hall();
+void loc_dining();
+void loc_toilet();
+void loc_upstairs();
+
 // INIT
-int answer;
-char name[20];
+int answer, location;
+int current_loc = 4;
 char* read;
-char* fword;
-char buffer[20];
+char name[40];
+char buffer[1024];
+
 
 
 // MAIN GAME
 int main()
 {
-    // TITLE SCREEN + INTRODUCTION
-    startUp();
+    startUp(); // INTRO
 
     // FIRST QUESTION
     printf("[1] look around\t [2] go north\t [3] go south\n\n");
     do {
         command();
-        fgets(buffer, sizeof(buffer), stdin);
-        sscanf_s(buffer, "%d", &answer);
-
-        if (!answer) // CHECK IF EMPTY
-            printf("Come again?\n\n");
-        else
+        if (!fgets(buffer, sizeof(buffer), stdin))
         {
-            switch (answer)
-            {
+            return 1;
+        }
+        answer = atoi(buffer);
+        switch (answer)
+        {
             case 1: printf("It's pouring, maybe it's better if we go somewhere to hide.\n\n"); break;
             case 2: printf("You go towards the mansion.\nThere's a sign on the door that says: \"Begone humans! Leave the dead in peace!\"\n\n"); break;
             case 3: printf("It's too dark going south, it's freezing outside.\n\n");
-                break;
-            default: printf("Answer %i is not an option.\n\n", answer); break;
-            }
+                    break;
+            default: printf("Come again? Please choose a number and hit enter.\n\n"); break;
         }
     } while (answer != 2);
 
@@ -58,9 +72,11 @@ int main()
 
     do {
         command();
-        fgets(buffer, sizeof(buffer), stdin);
-        sscanf_s(buffer, "%i", &answer);
-
+        if (!fgets(buffer, sizeof(buffer), stdin))
+        {
+            return 1;
+        }
+        answer = atoi(buffer);
         switch (answer)
         {
         case 1: printf("It's locked. You need to find a key.\n\n"); break;
@@ -68,7 +84,7 @@ int main()
         case 3: printf("You found the key under the flower pot!");
             printf("\nYou open the door, it's very dark inside..\n\n");
             break;
-        default: printf("Answer %i is not an option.\n\n", answer); break;
+        default: printf("Come again?\n\n"); break;
         }
     } while (answer != 3);
 
@@ -88,15 +104,17 @@ int main()
 
     do {
         command();
-        fgets(buffer, sizeof(buffer), stdin);
-        sscanf_s(buffer, "%i", &answer);
-
+        if (!fgets(buffer, sizeof(buffer), stdin))
+        {
+            return 1;
+        }
+        answer = atoi(buffer);
         switch (answer)
         {
         case 1: printf("Aaah that felt good!\n\n"); break;
         case 2: printf("Nothing happened.. I guess the power isn't working.\n\n"); break;
         case 3: printf("You found a candle, just in time. Your lighter was almost empty.\n"); break;
-        default: printf("Answer %i is not an option.\n", answer); break;
+        default: printf("Come again?\n\n"); break;
         }
     } while (answer != 3);
     printf("While looking for candles you also found a map which seems to be like the floorplan.\n");
@@ -108,39 +126,38 @@ int main()
     showmap();
 
     printf("\nChoose where you want to go to:\n");
-    printf("[1] KITCHEN     [2] DINING ROOM     [3] HAL     [4] LIVING ROOM     [5] UPSTAIRS\n\n");
+    // LOCATIONS
+    showmap();
+    printf("[1] %s     [2] %s     [3] %s     [4] %s     [5] %s     [6] %s\n\n", locs[1].desc_upper, locs[2].desc_upper, locs[3].desc_upper, locs[4].desc_upper, locs[5].desc_upper, locs[6].desc_upper);
     do
     {
         command();
-        fgets(buffer, sizeof(buffer), stdin);
-        sscanf_s(buffer, "%i", &answer);
-
-        switch (answer)
+        if (!fgets(buffer, sizeof(buffer), stdin)) // RETURNS IF NOT A NUMBER
         {
-        case 1:
-            printf("KITCHEN\n\n");
-            break;
-        case 2:
-            printf("DINING ROOM\n\n");
-            break;
-        case 3:
-            printf("HALL\n\n");
-            break;
-        case 4:
-            printf("LIVING ROOM\n\n");
-            break;
-        case 5:
-            printf("We haven't discovered all locations below yet.\n\n");
-            break;
-        default:
-            printf("Answer %i is not an option.\n\n", answer);
-            break;
+            return 1;
         }
-    } while (answer < 1 || answer > 4);
-    printf("Succes!\n");
-
+        answer = atoi(buffer);
+        
+        // CHECKS IF WE AREN'T IN THIS LOCATION ALREADY FIRST
+        if (current_loc == answer && answer != 0)
+            printf("You are already standing in the %s.\n\n", locs[answer].description);
+        else
+        {
+            switch (answer) // GO TO THE LOCATIONS
+            {
+                case 1: loc_kitchen();break;
+                case 2: loc_dining(); break;
+                case 3: loc_hall(); break;
+                case 4: loc_living(); break;
+                case 5: loc_toilet(); break;
+                case 6: loc_upstairs(); break;
+                default:
+                    printf("Come again?\n\n"); break;
+            }
+        }
+    } while (answer != 6);
+        printf("UPSTAIRS PART!\n");
 }
-
 
 
 // FUNCTIONS
@@ -194,8 +211,9 @@ char* fWord()
     char* input = buffer;
 
     fgets(input, sizeof(buffer), stdin);
+
     input[strcspn(input, "\r\n")] = 0; // REMOVES NEWLINE FROM GETLINE
-    input = strtok(input, " ");
+    input = strtok(input, " "); // ONLY READS UNTIL THE FIRST WHITESPACE
 
     return input;
 }
@@ -216,6 +234,47 @@ void showmap()
         " |             |  HALL  |                 |\n"
         " |_____________|________|_________________|\n"
     );
+}
+
+void loc_kitchen()
+{
+    
+    printf("KITCHEN\n\n");
+    current_loc = 1; // ADD LOCATION
+}
+
+void loc_dining()
+{
+    
+    printf("DINING ROOM\n\n");
+    current_loc = 2; // ADD LOCATION
+}
+
+void loc_hall()
+{
+    
+    printf("HALL\n\n");
+    current_loc = 3; // ADD LOCATION
+}
+
+void loc_living()
+{
+    
+    printf("LIVING\n\n");
+    current_loc = 4; // ADD LOCATION
+}
+
+void loc_toilet()
+{
+    printf("You sure have a small bladder!\n");
+    printf("Where do you want to go now?\n\n");
+    current_loc = 5; 
+}
+
+void loc_upstairs()
+{
+    printf("UPSTAIRS\n\n");
+    printf("You aren't allowed up here!\n\n");
 }
 
 // TITLE SCREEN & INTRODUCTION
